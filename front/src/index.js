96c105e377425d71081351c3881c36aa5df4e865
Akito Ito <akito0107@gmail.com>
2018-12-16 18:15:52 +0900

    update front

--- front/src/index.js
+++ front/src/index.js
@@ -1,4 +1,5 @@
 import * as monaco from "monaco-editor";
+import * as $ from "jquery";
 
 const gocode = `package main
 
@@ -8,8 +9,6 @@ func main() {
 	fmt.Println("hello, 世界")
 }
 `
-
-console.log('hoge')
 
 const editor = monaco.editor.create(document.getElementById("monaco-editor"), {
     value: gocode,
@@ -22,10 +21,23 @@ const input = document.getElementById("gosrc");
 const go = new Go();
 
 const model = editor.getModel();
+let rows = document.getElementsByClassName("astline");
 
-model.onDidChangeContent((event) => {
+model.onDidChangeContent(() => {
     input.value = model.getValue();
     WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
         go.run(result.instance);
+        rows = document.getElementsByClassName("astline");
     });
 });
+
+editor.onDidChangeCursorSelection((event) => {
+    const start = event.selection.startLineNumber
+    const end = event.selection.endLineNumber
+    $(".astline").removeClass("selected");
+
+    for (let i = start; i <= end; i++) {
+        $(`.lineno${i}`).addClass("selected");
+    }
+});
+
